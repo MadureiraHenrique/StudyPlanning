53192ce3e8b40b2a066c931ed7aca64710165e90
MadureiraHenrique <carloshenriquemadureira.santos@ucsal.edu.br>
2026-03-19 12:58:06 -0300

    init: project setup with Spring Boot and Next.js

---
+++ src/backend/main/java/com/kiza/studyplanning/StudyplanningApplication.java
@@ -0,0 +1,13 @@
+package com.kiza.studyplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyplanningApplication.class, args);
+	}
+
+}
